181a0ed36d3b33c968913bead7e5b8a9c0d9c81f
patrick_flege <patrick.flege@wur.nl>
2026-01-08 16:07:03 +0100

    Fixed potential error in logging to get string instead of unsigned integer

--- metagraph/src/cli/DeBruijnGraphWrapper.cpp
+++ metagraph/src/cli/DeBruijnGraphWrapper.cpp
@@ -196,7 +196,7 @@ const char * DeBruijnGraphWrapper::get_sequence_for_coords(std::string genome, u
      }
 
      std::cout << "RETRIEVED" << std::endl;
-     std::cout << current_kmer << std::endl;
+     std::cout << starting_kmer << std::endl;
 
      const  char * result = starting_kmer.c_str();
      return result;
